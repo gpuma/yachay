4c0a08d55a89fa1953e6af03df3b161fd0959e16
Gustavo Puma <vash47@gmail.com>
2018-02-01 21:04:29 -0500

    weighted avg done

--- ClientApp/components/unit/unit.ts
+++ ClientApp/components/unit/unit.ts
@@ -2,11 +2,23 @@ import Vue from 'vue';
 import { Component, Prop } from 'vue-property-decorator';
 
 //kind of a hack but there's not enough documentation 
-interface Unit { }
+interface Unit { 
+    name: string;
+    weight1: number;
+    weight2: number;
+    weight3: number;
+}
+interface Enrollment { 
+    grade1: number;
+    grade2: number;
+    grade3: number;
+}
 
 @Component
 export default class UnitComponent extends Vue {
-    unit: Unit = { };
+    //type assertion to avoid building an empty object
+    //since it will be replaced anyways by the fetched data
+    unit = <Unit>{}
 
     mounted() {
         fetch('api/units/'+this.$route.params.id)
@@ -14,5 +26,15 @@ export default class UnitComponent extends Vue {
             .then(data => {
                 this.unit = data;
             });
+    }
+
+    //weighted average
+    weightedAvg(e: Enrollment ) {
+        var wAvg = e.grade1 * this.unit.weight1 + 
+            e.grade2 * this.unit.weight2 +
+            e.grade3 * this.unit.weight3;
+
+        //one decimal place
+        return (Math.round(wAvg * 100) / 100).toFixed(1);
     }
 }
