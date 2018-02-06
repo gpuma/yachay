fba6865c6c4fda2bd63f84baca2338861927b27b
Gustavo Puma <vash47@gmail.com>
2018-02-06 18:03:39 -0500

    added a new base migration

---
+++ Migrations/20180206223405_InitialMigration.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace yachay.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Students",
+                columns: table => new
+                {
+                    StudentId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    FirstName = table.Column<string>(maxLength: 50, nullable: false),
+                    LastName = table.Column<string>(maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students", x => x.StudentId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Units",
+                columns: table => new
+                {
+                    UnitId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(maxLength: 100, nullable: false),
+                    Semester = table.Column<string>(maxLength: 50, nullable: false),
+                    Weight1 = table.Column<float>(nullable: false),
+                    Weight2 = table.Column<float>(nullable: false),
+                    Weight3 = table.Column<float>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Units", x => x.UnitId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Enrollments",
+                columns: table => new
+                {
+                    EnrollmentId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Grade1 = table.Column<float>(nullable: true),
+                    Grade2 = table.Column<float>(nullable: true),
+                    Grade3 = table.Column<float>(nullable: true),
+                    StudentId = table.Column<int>(nullable: false),
+                    UnitId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Enrollments", x => x.EnrollmentId);
+                    table.ForeignKey(
+                        name: "FK_Enrollments_Students_StudentId",
+                        column: x => x.StudentId,
+                        principalTable: "Students",
+                        principalColumn: "StudentId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Enrollments_Units_UnitId",
+                        column: x => x.UnitId,
+                        principalTable: "Units",
+                        principalColumn: "UnitId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Enrollments_StudentId",
+                table: "Enrollments",
+                column: "StudentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Enrollments_UnitId",
+                table: "Enrollments",
+                column: "UnitId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Enrollments");
+
+            migrationBuilder.DropTable(
+                name: "Students");
+
+            migrationBuilder.DropTable(
+                name: "Units");
+        }
+    }
+}
--- mock-data.sql
+++ mock-data.sql
@@ -1,4 +1,6 @@
-﻿insert into Students values('Melinda Lucía', 'Puente del Monte')
+﻿use yachay
+
+insert into Students values('Melinda Lucía', 'Puente del Monte')
 insert into Students values('James', 'Byrne')
 insert into Students values('Gustavo', 'Beltrán Linares')
 insert into Students values('Geoff', 'Sachs')
@@ -6,17 +8,17 @@ insert into Students values('Vito', 'Corleone')
 
 insert into Units values('Web Development', 'II', 0.3, 0.3, 0.4)
 insert into Units values('Introduction to Programming', 'I', 0.2, 0.2, 0.6)
+insert into Units values('Natural Language Processing', 'IX', 0.4, 0.4, 0.2)
+insert into Units values('Advanced Game Design', 'VI', 0.1, 0.1, 0.8)
 
-select * from Units
-select * from Students
-select * from Enrollments
-
-
-insert into Enrollments values(2,1,NULL,NULL,NULL)
-insert into Enrollments values(3,1,NULL,NULL,NULL)
-insert into Enrollments values(4,1,NULL,NULL,NULL)
+insert into Enrollments values(2,1,10,20,5)
+insert into Enrollments values(3,1,17,19,20)
+insert into Enrollments values(4,1,20,13,7)
 insert into Enrollments values(6,1,20,20,20)
 
+/*
+select * from Units
+select * from Students
 select * from Enrollments
 
 delete from Units
@@ -27,3 +29,4 @@ where studentid > 6
 
 update Enrollments
 set Grade3 = ABS(CHECKSUM(NewId())) % 20
+*/
